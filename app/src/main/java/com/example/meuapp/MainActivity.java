165606270d32f71bb0fed21ab9d81f7f459563a3
Yasmin Vieira <vieiracyasmin@gmail.com>
2026-04-09 12:12:18 -0300

    IMC

--- app/src/main/java/com/example/meuapp/MainActivity.java
+++ app/src/main/java/com/example/meuapp/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.meuapp;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
@@ -8,18 +9,48 @@ import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import org.w3c.dom.Text;
+
+import java.text.DecimalFormat;
 import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
+    Button btCaucular;
+    ImageView imageView;
+    EditText etPeso, etAltura;
+    TextView tvResultado, tvClass;
 
-
-    int posicao=0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        imageView=findViewById(R.id.imageView);
+        btCaucular=findViewById(R.id.btCalcular);
+        etPeso=findViewById(R.id.etPeso);
+        etAltura=findViewById(R.id.etAltura);
+        tvResultado=findViewById(R.id.tvResultado);
+        tvClass=findViewById(R.id.tvClass);
+
+
+        btCaucular.setOnClickListener(v -> {
+            String strPeso = etPeso.getText().toString();
+            String strAltura = etAltura.getText().toString();
+
+            Double peso = Double.parseDouble(strPeso);
+            Double altura = Double.parseDouble(strAltura);
+
+            Double imc = peso/(altura*altura);
+            DecimalFormat dc = new DecimalFormat("##.##");
+
+            tvResultado.setText(dc.format(imc));
+
+
+
+
+        });
 
 
 
