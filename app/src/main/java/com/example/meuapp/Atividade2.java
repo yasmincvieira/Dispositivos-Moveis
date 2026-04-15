e14548ff76accbf3a1e296e0cd92e1ce5bf89a6b
yasmincvieira <vieiracyasmin@gmail.com>
2026-04-15 11:34:36 -0300

    Atividade2

--- app/src/main/java/com/example/meuapp/Atividade2.java
+++ app/src/main/java/com/example/meuapp/Atividade2.java
@@ -1,7 +1,6 @@
 package com.example.meuapp;
 
 import android.os.Bundle;
-import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,22 +8,17 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-import java.io.File;
-
-public class MainActivity extends AppCompatActivity {
-
-    Button bntClick;
+public class Atividade2 extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
-
-        bntClick=findViewById(R.id.bntClick);
-        bntClick.setOnClickListener(v -> {
-
+        setContentView(R.layout.activity_atividade2);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
         });
-
     }
 }
