7455bae8d150c4ef03a41bf2d9af41f4635667bc
yasmincvieira <vieiracyasmin@gmail.com>
2026-04-30 11:45:54 -0300

    lista

--- app/src/main/java/com/example/meuapp/MainActivity.java
+++ app/src/main/java/com/example/meuapp/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.meuapp;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -12,11 +14,27 @@ import java.io.File;
 
 public class MainActivity extends AppCompatActivity {
 
+    String nomes[] = new String[]{"Daniel", "Sofia", "Prancha", "Davi", "Iran", "Fernando", "Borracha"};
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+
+        ListView listView = findViewById(R.id.listView);
+        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<>(
+                this, //Contexto
+                android.R.layout.simple_list_item_1, //Recurso de Layout xml
+                android.R.id.text1, //id do textView interno do layout
+                nomes //array de String DataSource
+        );
+
+        listView.setAdapter(arrayAdapter);
+        listView.setOnItemClickListener((parent, view, position, id) -> {
+
+        });
+
 
     }
 }
