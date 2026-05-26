05e3d0363c5bbbf6de644d8ca3ad183312f810b8
yasmincvieira <vieiracyasmin@gmail.com>
2026-05-26 09:57:01 -0300

    arrumando

--- app/src/main/java/com/example/meuapp/MainActivity.java
+++ app/src/main/java/com/example/meuapp/MainActivity.java
@@ -1,10 +1,14 @@
 package com.example.meuapp;
 
+import android.annotation.SuppressLint;
 import android.content.ContentValues;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ListView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -12,36 +16,49 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-import java.io.File;
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
-
     SQLiteDatabase sqLiteDatabase;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+        sqLiteDatabase = openOrCreateDatabase("notas",MODE_PRIVATE,null);
 
-        sqLiteDatabase = openOrCreateDatabase("notas", MODE_PRIVATE, null);
+        sqLiteDatabase.execSQL("CREATE TABLE IF NOT EXISTS notas (id INTEGER PRIMARY KEY AUTOINCREMENT,titulo TEXT,nota TEXT)");
+        //String titulo="João";
+        //sqLiteDatabase.execSQL("INSERT INTO notas VALUES(1,'"+titulo+"','')");
 
-        sqLiteDatabase.execSQL("CREATE TABLE IF NOT EXISTS notas (id INTEGER PRIMARY KEY AUTOINCREMENT, titulo TEXT, nota TEXT )");
 
-        //*  nomeColuna, valor  *
-        ContentValues contentValues = new ContentValues();
-        contentValues.put("titulo", "João");
-        contentValues.put("nota", "777777777");
-        sqLiteDatabase.insert("notas", null, contentValues);
+        /* nomeColuna, valor */
+        ContentValues contentValues= new ContentValues();
+        contentValues.put("titulo","joão");
+        contentValues.put("nota","7777777777");
+        sqLiteDatabase.insert("notas",null,contentValues);
+        ListView lv= findViewById(R.id.listview);
 
-        //recuperar dados do sqlite
-        Cursor c = sqLiteDatabase.rawQuery("SELECT id,id*10,titulo,nota FROM notas", null);
-        c.moveToFirst(); //movimenta o ponteiro do cursor para o primeiro registro recuperado
+        //Recuperar Dados do SQLITE
+        Cursor c=sqLiteDatabase.rawQuery("SELECT * FROM notas",null);
+        c.moveToFirst();//movimenta o ponteiro do cursor para o primeiro registro recuperado
+        ArrayList<Nota> listaNotas = new ArrayList<>();
+        while(!c.isAfterLast()) {
+            @SuppressLint("Range") int id = c.getInt(c.getColumnIndex("id"));
+            @SuppressLint("Range") String titulo = c.getString(c.getColumnIndex("titulo"));
+            @SuppressLint("Range") String nota = c.getString(c.getColumnIndex("nota"));
+            Nota n = new  Nota(id,titulo,nota);
+            listaNotas.add(n);
+            Log.d("SELECT", Integer.toString(id) + "," + titulo + "," + nota);
+            c.moveToNext();
+        }
+        ArrayList<String> listaTitulos = new ArrayList<>();
+        for(Nota nota:listaNotas){
+            listaTitulos.add(nota.titulo);
+        }
+        ArrayAdapter<String> adapter= new ArrayAdapter<>(this, android.R.layout.simple_list_item_1,android.R.id.text1,listaTitulos);
+        lv.setAdapter(adapter);
 
-        int id = c.getInt(c.getColumnIndex("id"));
-        String titulo = c.getString(c.getColumnIndex("titulo"));
-        String nota = c.getString(c.getColumnIndex("nota"));
-        Log.d("SELECT", Integer.toString(id)+titulo+","+nota);
 
     }
 }
---
+++ app/src/main/java/com/example/meuapp/Nota.java
@@ -0,0 +1,13 @@
+package com.example.meuapp;
+
+public class Nota {
+    Integer id;
+    String titulo;
+    String nota;
+
+    public Nota(Integer id, String titulo, String nota) {
+        this.id = id;
+        this.titulo = titulo;
+        this.nota = nota;
+    }
+}
